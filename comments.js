53e0ce2559a7cf17152fa659fb5f319f72f9608c
Andrea <a.eliza1992@gmail.com>
2023-07-26 16:25:41 -0500

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,59 @@
+// Create web server
+// Open terminal
+// $ npm init
+// $ npm install express --save
+// $ npm install nodemon --save-dev
+// $ npm install ejs --save
+// $ npm install body-parser --save
+// $ npm install mongoose --save
+// $ npm install express-session --save
+// $ npm install express-validator --save
+// $ npm install bcryptjs --save
+// $ npm install passport --save
+// $ npm install passport-local --save
+// $ npm install connect-flash --save
+// $ npm install multer --save
+// $ npm install method-override --save
+// $ npm install lodash --save
+// $ npm install moment --save
+// $ npm install express-fileupload --save
+// $ npm install dotenv --save
+// $ npm install socket.io --save
+// $ npm install --save-dev nodemon
+// $ touch app.js
+// $ touch .env
+// $ touch .gitignore
+// $ mkdir config
+// $ touch config/database.js
+// $ mkdir routes
+// $ touch routes/index.js
+// $ mkdir models
+// $ touch models/user.js
+// $ mkdir views
+// $ mkdir views/partials
+// $ touch views/partials/header.ejs
+// $ touch views/partials/footer.ejs
+// $ mkdir views/layouts
+// $ touch views/layouts/layout.ejs
+// $ mkdir views/pages
+// $ touch views/pages/index.ejs
+// $ touch views/pages/about.ejs
+// $ mkdir views/pages/users
+// $ touch views/pages/users/login.ejs
+// $ touch views/pages/users/register.ejs
+// $ touch views/pages/users/profile.ejs
+// $ touch views/pages/users/edit-profile.ejs
+// $ touch views/pages/users/edit-password.ejs
+// $ touch views/pages/users/forgot.ejs
+// $ touch views/pages/users/reset.ejs
+// $ touch views/pages/users/confirm.ejs
+// $ touch views/pages/users/confirm-password.ejs
+// $ touch views/pages/users/confirm-reset.ejs
+// $ mkdir views/pages/users/admin
+// $ touch views/pages/users/admin/index.ejs
+// $ touch views/pages/users/admin/add.ejs
+// $ touch views/pages/users/admin/edit.ejs
+// $ touch views/pages/users/admin/delete.ejs
+// $ touch views/pages/users/admin/profile.ejs
+// $ touch views/pages/users/admin/edit-profile.ejs
+// $ touch views/pages/users
